d366669f81532a522d34599ddb8ab41aaa6faf82
Slotigork <maxsoneback@hotmail.com>
2018-11-06 18:52:29 +0100

    försök till array

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,13 +1,24 @@
-let vm = new Vue({});
-function MenuItem(b_name, kcal, allergen, freeFromX){
+function MenuItem(b_name, kcal, containsGluten, containsLactose){
   this.name = b_name;
-  this.cal=kcal;
-  this.allergies=allergen;
-  this.freeFrom=freeFromX;
+  this.kcal=kcal;
+  this.containsGluten = containsGluten;
+  this.containsLactose = containsLactose;
+
   this.returnNameAndCal=function(){
     return this.name + ' contains ' + this.cal + ' kCal';
   };
 }
 
-let myBurger = new MenuItem("Fire Burger", 765, "mercury", "gluten" );
-//console.log(myBurger.returnNameAndCal());
+let fireBurger = new MenuItem("Fire Burger", 2050, true, true );
+let waterBurger = new MenuItem("Water Burger", 574, false, true );
+let maxBurger = new MenuItem("Max Burger", 300, true, true );
+let brownBurger = new MenuItem("Brown Burger", 1012, false, false );
+let sweetBurger = new MenuItem("Sweet Burger", 7014, true, false );
+
+let burgers = new Array(fireBurger, waterBurger, maxBurger, brownBurger, sweetBurger);
+
+let vm = new Vue({
+el:"#burgerwrapper",
+data:{}
+}
+);
