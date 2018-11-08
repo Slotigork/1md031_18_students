354c72ad910891bb268065f6ee8c569ab1efbaf1
Slotigork <maxsoneback@hotmail.com>
2018-11-08 21:38:30 +0100

    pickedBurger funkar ej

--- public/js/bttn-listener.js
+++ public/js/bttn-listener.js
@@ -1,5 +1,15 @@
 let bttn_listener = new Vue({
-el:"#submit-bttn",
+el:"#order-page",
+data:{
+  pickedBurgers: [],
+  fullname: '',
+  email: '',
+  streetname: '',
+  housenum: undefined,
+  payment: '',
+  gender: 'Undisclosed',
+
+},
 methods: {
   markDone: function(){
     console.log("Button has been clicked!");
